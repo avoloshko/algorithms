18a89d4e47a66f1c14f340250d5f9abddcdb85b4
Alexander Voloshko <alexander.voloshko@gmail.com>
2018-01-11 09:53:41 +0000

    Update TwoThreeTree.java

--- src/main/java/com/voloshko/algorithms/sort/TwoThreeTree.java
+++ src/main/java/com/voloshko/algorithms/sort/TwoThreeTree.java
@@ -10,21 +10,22 @@ import javax.xml.soap.Node;
  * Search	O(log n)	O(log n)
  * Insert	O(log n)	O(log n)
  * Delete	O(log n)	O(log n)
+ *
  * @author avoloshko
  */
 public class TwoThreeTree<K extends Comparable<K>, V> {
 
-	private Node root;
-	private int size;
+    private Node root;
+    private int size;
 
-	private static class Pair<K, V> {
-		Pair(K key, V value) {
-			this.key = key;
-			this.value = value;
-		}
+    private static class Pair<K, V> {
+        Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
 
-		K key;
-		V value;
+        K key;
+        V value;
 
         @Override
         public String toString() {
@@ -32,208 +33,208 @@ public class TwoThreeTree<K extends Comparable<K>, V> {
         }
     }
 
-	private class Node {
+    private class Node {
 
         Node(Pair<K, V> pair) {
             kvPairLeft = pair;
         }
 
 
-		Pair<K, V> kvPairLeft, kvPairRight;
+        Pair<K, V> kvPairLeft, kvPairRight;
 
         Node nodeLeft, nodeMiddle, nodeRight;
 
         void addLeft(Pair<K, V> pair) {
-			kvPairRight = kvPairLeft;
-			kvPairLeft = pair;
+            kvPairRight = kvPairLeft;
+            kvPairLeft = pair;
         }
 
-		void addRight(Pair<K, V> pair) {
-			kvPairRight = pair;
-		}
-	}
+        void addRight(Pair<K, V> pair) {
+            kvPairRight = pair;
+        }
+    }
 
-	public void remove(K key, V value) {
+    public void remove(K key, V value) {
 
-	}
+    }
 
-	public void get(K key, V value) {
+    public void get(K key, V value) {
 
-		// try to find
-		Pair<K, V> pair = find(root, key);
-		if (pair != null) {
-			pair.value = value;
-			return;
-		}
+        // try to find
+        Pair<K, V> pair = find(root, key);
+        if (pair != null) {
+            pair.value = value;
+            return;
+        }
 
-		// insert
-		if (root == null) {
-			root = new Node(new Pair<>(key, value));
-		} else {
-			Node result = put(root, new Pair<>(key, value));
-			if (result != null) {
-				// root was changed
-				root = result;
-			}
-		}
+        // insert
+        if (root == null) {
+            root = new Node(new Pair<>(key, value));
+        } else {
+            Node result = put(root, new Pair<>(key, value));
+            if (result != null) {
+                // root was changed
+                root = result;
+            }
+        }
 
-		size += 1;
-	}
+        size += 1;
+    }
 
-	private Node splitWithLeftNode(Node original, Node leftNode) {
-		Node middleNode = new Node(original.kvPairLeft);
+    private Node splitWithLeftNode(Node original, Node leftNode) {
+        Node middleNode = new Node(original.kvPairLeft);
 
-		Node rightNode = new Node(original.kvPairRight);
-		rightNode.nodeLeft = original.nodeMiddle;
-		rightNode.nodeRight = original.nodeRight;
+        Node rightNode = new Node(original.kvPairRight);
+        rightNode.nodeLeft = original.nodeMiddle;
+        rightNode.nodeRight = original.nodeRight;
 
-		middleNode.nodeLeft = leftNode;
-		middleNode.nodeRight = rightNode;
+        middleNode.nodeLeft = leftNode;
+        middleNode.nodeRight = rightNode;
 
-		return middleNode;
-	}
+        return middleNode;
+    }
 
-	private Node splitWithMiddleNode(Node original, Node newNode) {
-		Node middleNode = new Node(newNode.kvPairLeft);
+    private Node splitWithMiddleNode(Node original, Node newNode) {
+        Node middleNode = new Node(newNode.kvPairLeft);
 
-		Node leftNode = new Node(original.kvPairLeft);
-		leftNode.nodeLeft = original.nodeLeft;
-		leftNode.nodeRight = newNode.nodeLeft;
+        Node leftNode = new Node(original.kvPairLeft);
+        leftNode.nodeLeft = original.nodeLeft;
+        leftNode.nodeRight = newNode.nodeLeft;
 
-		Node rightNode = new Node(original.kvPairRight);
-		rightNode.nodeLeft = newNode.nodeRight;
-		rightNode.nodeRight = original.nodeRight;
+        Node rightNode = new Node(original.kvPairRight);
+        rightNode.nodeLeft = newNode.nodeRight;
+        rightNode.nodeRight = original.nodeRight;
 
-		middleNode.nodeLeft = leftNode;
-		middleNode.nodeRight = rightNode;
+        middleNode.nodeLeft = leftNode;
+        middleNode.nodeRight = rightNode;
 
-		return middleNode;
-	}
+        return middleNode;
+    }
 
-	private Node splitWithRightNode(Node original, Node rightNode) {
-		Node middleNode = new Node(original.kvPairRight);
+    private Node splitWithRightNode(Node original, Node rightNode) {
+        Node middleNode = new Node(original.kvPairRight);
 
-		Node leftNode = new Node(original.kvPairLeft);
-		leftNode.nodeLeft = original.nodeLeft;
-		leftNode.nodeRight = original.nodeMiddle;
+        Node leftNode = new Node(original.kvPairLeft);
+        leftNode.nodeLeft = original.nodeLeft;
+        leftNode.nodeRight = original.nodeMiddle;
 
-		middleNode.nodeLeft = leftNode;
-		middleNode.nodeRight = rightNode;
+        middleNode.nodeLeft = leftNode;
+        middleNode.nodeRight = rightNode;
 
-		return middleNode;
-	}
+        return middleNode;
+    }
 
-	private void mergeWithRight(Node original, Node newNode) {
-		original.kvPairRight = newNode.kvPairLeft;
-		original.nodeMiddle = newNode.nodeLeft;
-		original.nodeRight = newNode.nodeRight;
-	}
+    private void mergeWithRight(Node original, Node newNode) {
+        original.kvPairRight = newNode.kvPairLeft;
+        original.nodeMiddle = newNode.nodeLeft;
+        original.nodeRight = newNode.nodeRight;
+    }
 
-	private void mergeWithLeft(Node original, Node newNode) {
-		original.kvPairRight = original.kvPairLeft;
-		original.kvPairLeft = newNode.kvPairLeft;
-		original.nodeLeft = newNode.nodeLeft;
-		original.nodeMiddle = newNode.nodeRight;
-	}
+    private void mergeWithLeft(Node original, Node newNode) {
+        original.kvPairRight = original.kvPairLeft;
+        original.kvPairLeft = newNode.kvPairLeft;
+        original.nodeLeft = newNode.nodeLeft;
+        original.nodeMiddle = newNode.nodeRight;
+    }
 
-	private Node put(Node node, Pair<K, V> pair) {
-		int leftCompare = pair.key.compareTo(node.kvPairLeft.key);
+    private Node put(Node node, Pair<K, V> pair) {
+        int leftCompare = pair.key.compareTo(node.kvPairLeft.key);
 
-		if (node.kvPairRight == null) {
-			// single node
-			if (leftCompare < 0) {
-				if (node.nodeLeft != null) {
-					Node newNode = put(node.nodeLeft, pair);
-					if (newNode != null) {
-						// a split occurred lower the tree
-						mergeWithLeft(node, newNode);
-						return null;
-					}
-				} else {
-					node.addLeft(pair);
-					return null;
-				}
-			} else {
-				if (node.nodeRight != null) {
-					Node newNode = put(node.nodeRight, pair);
-					if (newNode != null) {
-						// a split occurred lower the tree
-						mergeWithRight(node, newNode);
-						return null;
-					}
-				} else {
-					node.addRight(pair);
-					return null;
-				}
-			}
-		} else {
-			// double node
-			if (leftCompare < 0) {
-				if (node.nodeLeft != null) {
-					Node newNode = put(node.nodeLeft, pair);
-					if (newNode != null) {
-						// a split occurred lower the tree and here we should also split the node
-						return splitWithLeftNode(node, newNode);
-					}
-				} else {
-					return splitWithLeftNode(node, new Node(pair));
-				}
-			} else {
-				int rightCompare = pair.key.compareTo(node.kvPairRight.key);
+        if (node.kvPairRight == null) {
+            // single node
+            if (leftCompare < 0) {
+                if (node.nodeLeft != null) {
+                    Node newNode = put(node.nodeLeft, pair);
+                    if (newNode != null) {
+                        // a split occurred lower the tree
+                        mergeWithLeft(node, newNode);
+                        return null;
+                    }
+                } else {
+                    node.addLeft(pair);
+                    return null;
+                }
+            } else {
+                if (node.nodeRight != null) {
+                    Node newNode = put(node.nodeRight, pair);
+                    if (newNode != null) {
+                        // a split occurred lower the tree
+                        mergeWithRight(node, newNode);
+                        return null;
+                    }
+                } else {
+                    node.addRight(pair);
+                    return null;
+                }
+            }
+        } else {
+            // double node
+            if (leftCompare < 0) {
+                if (node.nodeLeft != null) {
+                    Node newNode = put(node.nodeLeft, pair);
+                    if (newNode != null) {
+                        // a split occurred lower the tree and here we should also split the node
+                        return splitWithLeftNode(node, newNode);
+                    }
+                } else {
+                    return splitWithLeftNode(node, new Node(pair));
+                }
+            } else {
+                int rightCompare = pair.key.compareTo(node.kvPairRight.key);
 
-				if (rightCompare > 0) {
-					if (node.nodeRight != null) {
-						Node newNode = put(node.nodeRight, pair);
-						if (newNode != null) {
-							// a split occurred lower the tree and here we should also split the node
-							return splitWithRightNode(node, newNode);
-						}
-					} else {
-						return splitWithRightNode(node, new Node(pair));
-					}
-				} else {
-					if (node.nodeMiddle != null) {
-						Node newNode = put(node.nodeMiddle, pair);
-						if (newNode != null) {
-							// a split occurred lower the tree and here we should also split the node
-							return splitWithMiddleNode(node, newNode);
-						}
-					} else {
-						return splitWithMiddleNode(node, new Node(pair));
-					}
-				}
-			}
-		}
-		return null;
-	}
+                if (rightCompare > 0) {
+                    if (node.nodeRight != null) {
+                        Node newNode = put(node.nodeRight, pair);
+                        if (newNode != null) {
+                            // a split occurred lower the tree and here we should also split the node
+                            return splitWithRightNode(node, newNode);
+                        }
+                    } else {
+                        return splitWithRightNode(node, new Node(pair));
+                    }
+                } else {
+                    if (node.nodeMiddle != null) {
+                        Node newNode = put(node.nodeMiddle, pair);
+                        if (newNode != null) {
+                            // a split occurred lower the tree and here we should also split the node
+                            return splitWithMiddleNode(node, newNode);
+                        }
+                    } else {
+                        return splitWithMiddleNode(node, new Node(pair));
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
-	public V find(K key) {
-		Pair<K, V> pair = find(root, key);
-		if (pair != null) {
-			return pair.value;
-		}
-		return null;
-	}
+    public V find(K key) {
+        Pair<K, V> pair = find(root, key);
+        if (pair != null) {
+            return pair.value;
+        }
+        return null;
+    }
 
-	private Pair<K, V> find(Node node, K key) {
-		if (node == null) {
-			return null;
-		}
+    private Pair<K, V> find(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
 
-		int compare = node.kvPairLeft.key.compareTo(key);
-		if (compare == 0) {
-			return node.kvPairLeft;
-		}
+        int compare = node.kvPairLeft.key.compareTo(key);
+        if (compare == 0) {
+            return node.kvPairLeft;
+        }
 
-		if (compare > 0) {
+        if (compare > 0) {
             if (node.nodeLeft != null) {
                 return find(node.nodeLeft, key);
             }
 
             return null;
-		}
+        }
 
-		if (node.kvPairRight != null) {
+        if (node.kvPairRight != null) {
             compare = node.kvPairRight.key.compareTo(key);
             if (compare == 0) {
                 return node.kvPairRight;
@@ -250,28 +251,28 @@ public class TwoThreeTree<K extends Comparable<K>, V> {
             if (node.nodeMiddle != null) {
                 return find(node.nodeMiddle, key);
             }
-		} else {
-			if (node.nodeRight != null) {
-				return find(node.nodeRight, key);
-			}
-		}
+        } else {
+            if (node.nodeRight != null) {
+                return find(node.nodeRight, key);
+            }
+        }
 
         return null;
-	}
+    }
 
-	public int size() {
-		return size;
-	}
+    public int size() {
+        return size;
+    }
 
-	public int maxHeight() {
-		return maxHeight(root);
-	}
+    public int maxHeight() {
+        return maxHeight(root);
+    }
 
-	private int maxHeight(Node node) {
-		if (node == null) {
-			return 0;
-		}
+    private int maxHeight(Node node) {
+        if (node == null) {
+            return 0;
+        }
 
-		return 1 + Math.max(Math.max(maxHeight(node.nodeLeft), maxHeight(node.nodeMiddle)), maxHeight(node.nodeRight));
-	}
+        return 1 + Math.max(Math.max(maxHeight(node.nodeLeft), maxHeight(node.nodeMiddle)), maxHeight(node.nodeRight));
+    }
 }
