1af9ad440e7146cc4784414c9d2812aebf85a409
Alexander Voloshko <alexander.voloshko@gmail.com>
2018-01-11 09:56:48 +0000

    Update LevenshteinDistanceTest.java

--- src/test/java/com/voloshko/algorithms/LevenshteinDistanceTest.java
+++ src/test/java/com/voloshko/algorithms/LevenshteinDistanceTest.java
@@ -7,15 +7,15 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class LevenshteinDistanceTest extends TestCase {
-	public LevenshteinDistanceTest(String testName) {
-		super(testName);
-	}
+    public LevenshteinDistanceTest(String testName) {
+        super(testName);
+    }
 
-	public static Test suite() {
-		return new TestSuite(LevenshteinDistanceTest.class);
-	}
+    public static Test suite() {
+        return new TestSuite(LevenshteinDistanceTest.class);
+    }
 
-	public void test() {
-		assertTrue(LevenshteinDistance.findDistance("aazzzaaarrryy", "aatzzzbbarryy") == 4);
-	}
+    public void test() {
+        assertTrue(LevenshteinDistance.findDistance("aazzzaaarrryy", "aatzzzbbarryy") == 4);
+    }
 }
